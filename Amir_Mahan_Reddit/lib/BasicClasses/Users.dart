b0be24ca29e2fad203475ae95b904533d29d0179
AmirMohammad Piran <amirdanger80p@gmail.com>
2022-05-25 16:40:45 +0430

    adding some functions and fields like LikedPosts

--- Amir_Mahan_Reddit/lib/BasicClasses/Users.dart
+++ Amir_Mahan_Reddit/lib/BasicClasses/Users.dart
@@ -1,8 +1,12 @@
+import 'package:Amir_Mahan_Reddit/BasicClasses/Community.dart';
 import 'package:Amir_Mahan_Reddit/BasicClasses/Post.dart';
 
 class User {
   String username;
   List<Post> savedPosts;
+  List<Post> likedPosts;
+  List<Post> dislikedPosts;
+  List<Community> favoriteCommunities;
   String _email;
   String _password;
   int numPosts;
@@ -16,7 +20,42 @@ class User {
     this.numPosts = 0;
     this.createdAt = DateTime.now().toString();
     this.isMale = isMale;
+    this.likedPosts = <Post>[];
+    this.dislikedPosts = <Post>[];
+    this.favoriteCommunities = <Community>[];
   }
+  void addLike(Post likedPost) {
+    this.likedPosts.add(likedPost);
+  }
+
+  void addDislike(Post dislikedPost) {
+    this.dislikedPosts.add(dislikedPost);
+  }
+
+  void addSavedPost(Post savedPost) {
+    this.savedPosts.add(savedPost);
+  }
+
+  void addFavoriteCommunity(Community community) {
+    this.favoriteCommunities.add(community);
+  }
+
+  bool hasLiked(Post post) {
+    return this.likedPosts.contains(post);
+  }
+
+  bool hasDisliked(Post post) {
+    return this.dislikedPosts.contains(post);
+  }
+
+  bool hasSaved(Post post) {
+    return this.savedPosts.contains(post);
+  }
+
+  bool hasSetFavoriteCommunity(Community community) {
+    return this.favoriteCommunities.contains(community);
+  }
+
   String getEmail() {
     return _email;
   }
@@ -39,5 +78,17 @@ class User {
 
   String getUsername() {
     return username;
+  }
+
+  List<Post> getLikedPosts() {
+    return likedPosts;
+  }
+
+  List<Post> getDislikedPosts() {
+    return dislikedPosts;
+  }
+
+  List<Community> getFavoriteCommunities() {
+    return favoriteCommunities;
   }
 }
