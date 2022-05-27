2c7289bb37df01ff3ee16fa84bf84c0411d71e9b
Mahan Veisi <mahan8292@gmail.com>
2022-05-27 16:19:51 +0430

    updating profile

--- Amir_Mahan_Reddit/lib/Screens/OnSidebar/ProfilePage.dart
+++ Amir_Mahan_Reddit/lib/Screens/OnSidebar/ProfilePage.dart
@@ -30,8 +30,9 @@ class _UserPageState extends State<UserPage> {
         backgroundColor: Colors.transparent,
       ),
       extendBodyBehindAppBar: true,
+      backgroundColor: Color(0xFF1F2833),
       body: SingleChildScrollView(
-        child: Stack(
+        child: Column(
           children: [
             SizedBox(
               height: MediaQuery.of(context).size.height/2,
@@ -48,49 +49,167 @@ class _UserPageState extends State<UserPage> {
                 ),
               ),
             ),
-            Positioned(
-              top: MediaQuery.of(context).size.height/2 * 0.80,
-              left: MediaQuery.of(context).size.width/2 * 0.12,
-              child:  Container(
-                width: MediaQuery.of(context).size.width,
-                height: 50,
-                child: Text(
-                  'Sarah Absdsfsfsads',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+              Positioned(
+                top: MediaQuery.of(context).size.height/2 * 0.8,
+                left: MediaQuery.of(context).size.width/2 * 0.20,
+                child:  Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 18,
+                    vertical: 5
+                  ),
+                  child: Text(
+                    'Sarah Absdsfsfsads',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFF5F5F5),
+                    ),
+                  ),
                   ),
                 ),
-                ),
-              ),
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.7,
+            SizedBox(
+              height: 1,
+              width: MediaQuery.of(context).size.width * 0.9,
               child: Container(
-                width: MediaQuery.of(context).size.width,
-                // height: ,
-                height: 200,
-                // width: 300 ,
-                // padding: EdgeInsets.all(10),
-                // decoration: BoxDecoration(
-                //   borderRadius: BorderRadius.only(
-                //     bottomLeft: Radius.circular(30),
-                //     bottomRight: Radius.circular(30),
-                //   ),
-                //   color: Colors.white,
-                // ),
-                child: Text(
-                  'Sarah Absdsfsfsads',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                color: Color(0xFF66FCF1),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              height: 400,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Container(
+                    // backgroundColor: Color(0xFF1F2833),
+                    // color: Color(0xFF1F2833),
+                    height: 200,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      // color: Colors.white,
+                     color: Color(0xFF1F2833),
+                      border: Border.all(
+                        color: Color(0xFF66FCF1),
+                        width: 2,
+                      ),
+                    ),
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 10, 10, 0),
+                          height: 35,
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            'Account',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                          // margin: EdgeInsets.only(left: 10),
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.person,
+                                color: Color(0xFF66FCF1),
+                                size: 30,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                'Username : ',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'Alli Sherlock',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFC5C6C7),
+                                ),
+                              ),
+                              ],
+                          )
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                          // margin: EdgeInsets.only(left: 10),
+                          height: 30,
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.email,
+                                color: Color(0xFF66FCF1),
+                                size: 30,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                'Email : ',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'ali@gmail.com',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFC5C6C7),
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'My Posts',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                          ],
+                        ),
+                      ),
+                    ]
                   ),
                 ),
-              )
-              ),
-           ]
-         ),
+          ],
+        ),
       ),
     );
   }
