3c7bfefac81ad53b66b8128da5539de9dfde2532
Mahan Veisi <mahan8292@gmail.com>
2022-06-02 19:28:19 +0430

    improving side bar drawer

--- Amir_Mahan_Reddit/lib/Widgets/sideBarDrawer.dart
+++ Amir_Mahan_Reddit/lib/Widgets/sideBarDrawer.dart
@@ -34,46 +34,46 @@ class NavigationDrawerWidget extends StatelessWidget {
               padding: padding,
               child: Column(
                 children: [
-                  const SizedBox(height: 12),
-                  buildSearchField(),
+                  // const SizedBox(height: 12),
+                  // buildSearchField(),
                   const SizedBox(height: 24),
                   buildMenuItem(
                     text: 'Profile',
                     icon: Icons.person,
                     onClicked: () => selectedItem(context, 0),
                   ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 10),
                   buildMenuItem(
                     text: 'Saved',
                     icon: Icons.favorite,
                     onClicked: () => selectedItem(context, 1),
                   ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 10),
                   buildMenuItem(
                     text: 'Theme',
                     icon: Icons.color_lens,
                     onClicked: () => selectedItem(context, 2),
                   ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 10),
                   buildMenuItem(
                     text: 'Settings',
                     icon: Icons.settings,
                     onClicked: () => selectedItem(context, 3),
                   ),
-                  const SizedBox(height: 24),
-                  Divider(color: Colors.white70),
-                  const SizedBox(height: 24),
+                  // const SizedBox(height: 24),
+                  // Divider(color: Colors.white70),
+                  const SizedBox(height: 10),
                   buildMenuItem(
                     text: 'About Us',
                     icon: Icons.info,
                     onClicked: () => selectedItem(context, 4),
                   ),
-                  const SizedBox(height: 16),
-                  buildMenuItem(
-                    text: 'Notifications',
-                    icon: Icons.notifications_outlined,
-                    onClicked: () => selectedItem(context, 5),
-                  ),
+                  // const SizedBox(height: 10),
+                  // buildMenuItem(
+                  //   text: 'Notifications',
+                  //   icon: Icons.notifications_outlined,
+                  //   onClicked: () => selectedItem(context, 5),
+                  // ),
                 ],
               ),
             ),
@@ -91,61 +91,35 @@ class NavigationDrawerWidget extends StatelessWidget {
   }) =>
       InkWell(
         onTap: onClicked,
-        child: Container(
-          padding: padding.add(EdgeInsets.symmetric(vertical: 40)),
-          child: Row(
-            children: [
-              CircleAvatar(radius: 30, backgroundImage: NetworkImage(urlImage)),
-              SizedBox(width: 20),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    name,
-                    style: TextStyle(fontSize: 20, color: Colors.white),
+        child: Column(
+          children: [
+            Container(
+              padding: padding.add(EdgeInsets.fromLTRB(0, 40, 0, 20)),
+              child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage(
+                    "assets/images/morty.png",
                   ),
-                  const SizedBox(height: 4),
-                  Text(
-                    email,
-                    style: TextStyle(fontSize: 14, color: Colors.white),
-                  ),
-                ],
+                backgroundColor: Colors.transparent,
               ),
-              Spacer(),
-              CircleAvatar(
-                radius: 24,
-                backgroundColor: Color.fromARGB(236, 27, 241, 234),
-
-                child: Icon(Icons.add_comment_outlined, color: Colors.white),
-              )
-            ],
-          ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  email,
+                  style: TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ],
+            ),
+          ],
         ),
       );
-
-  Widget buildSearchField() {
-    final color = Colors.white;
-
-    return TextField(
-      style: TextStyle(color: color),
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        hintText: 'Search',
-        hintStyle: TextStyle(color: color),
-        prefixIcon: Icon(Icons.search, color: color),
-        filled: true,
-        fillColor: Colors.white12,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide(color: color.withOpacity(0.7)),
-        ),
-      ),
-    );
-  }
 
   Widget buildMenuItem({
     String text,
@@ -169,7 +143,6 @@ class NavigationDrawerWidget extends StatelessWidget {
     switch (index) {
       case 0:
         Navigator.of(context).push(MaterialPageRoute(
-          // builder: (context) => UserPage(name: 'Sarah Abs', urlImage: 'https://pbs.twimg.com/media/E7CdHG3WQAANrwu.jpg'),
           builder: (context) => UserPage(),
         )
         );
