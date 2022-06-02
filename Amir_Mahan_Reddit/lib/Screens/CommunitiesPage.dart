b966bd3da159e90ed4965b468609f247892ce70a
Mahan Veisi <mahan8292@gmail.com>
2022-06-02 18:04:45 +0430

    search field for community page added

--- Amir_Mahan_Reddit/lib/Screens/CommunitiesPage.dart
+++ Amir_Mahan_Reddit/lib/Screens/CommunitiesPage.dart
@@ -66,6 +66,16 @@ class _CommunitiesPageState extends State<CommunitiesPage> {
               fontWeight: FontWeight.bold,
             ),
           ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search, color: Colors.white),
+                onPressed: () {
+                  showSearch(
+                    context: context,
+                    delegate: SearchBox(),
+                  );
+                })
+          ],
         ),
         body: Container(
           child: ListView.builder(
@@ -207,5 +217,179 @@ class _CommunitiesPageState extends State<CommunitiesPage> {
             ],
           )),
         ));
+  }
+}
+
+class SearchBox extends SearchDelegate {
+  List<Community> communities = <Community>[];
+  void initializer() {
+    communities.add(new Community(
+        new User("Setareh", "email", "password", false),
+        "Logical_Circuits",
+        "Here's a community for people who like to discuss logical circuits! Feel free to ask questions, and join the discussion!"));
+    communities.add(new Community(new User("Amir", "email", "password", true),
+        "Foosball_Kings", "Soccer is cool, but it's cooler on a table!"));
+    communities.add(new Community(
+        new User("Mahan", "email", "password", true),
+        "Eminem_Fans_Society",
+        "We're here to celebrate the fandom of our favourite singer! Join us!"));
+    communities.add(new Community(
+        new User("Matin", "email", "password", true),
+        "IMDB_Eaters",
+        "This is a community for Movies and Series Lovers! We love to discuss them!"));
+    communities.add(new Community(
+        new User("Amir", "email", "password", true),
+        "Metalheads_Club",
+        "Welcome to Metalheads Club! Don't forget Horns-ups!"));
+    communities.add(new Community(new User("Sobhan", "email", "password", true),
+        "Call_Of_Duty_Noobs", "If you're a noob like us, join us!"));
+    communities.add(new Community(new User("Mahan", "email", "password", true),
+        "Respect club", "here we respect to each other\'s beliefs and opinions, we do not"
+            "insult other\'s interests such as music favorites and so much more that"
+            "makes other people feel uncomfortable...\n please note there isn\'t an"
+            "anti-Amir mohammad piran club"));
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          if(query.isEmpty){
+            close(context, null);
+          }
+          else{
+            query = "";
+          }
+        },
+      )
+    ];
+  }
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<Community> filteredCommunities = communities.where((community) {
+      final nameResult = community.name.toLowerCase();
+      final descriptionResult = community.description;
+      final input = query.toLowerCase();
+      return nameResult.contains(input);
+    }).toList();
+    initializer();
+    return Scaffold(
+      body: Container(
+        child: ListView.builder(
+            itemCount: filteredCommunities.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (c, a1, a2) => CommunitiesDetailsPage(
+                          community: filteredCommunities[index],
+                        ),
+                        transitionsBuilder: (c, anim, a2, child) =>
+                            FadeTransition(opacity: anim, child: child),
+                        transitionDuration: Duration(milliseconds: 200),
+                      ),
+                    );
+                  },
+                  child: Container(
+                      color: Colors.transparent,
+                      child: Material(
+                          color: Colors.transparent,
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.only(
+                                            right: 10,
+                                            left: 10,
+                                            top: 15,
+                                            bottom: 10),
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.transparent,
+                                          backgroundImage: AssetImage(
+                                              "assets/images/Doofus_Rick.png"),
+                                        ),
+                                      ),
+                                      Container(
+                                        child: Text(
+                                            "r/" +
+                                                filteredCommunities[index].getName(),
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Gotham',
+                                              fontSize: 18,
+                                            )),
+                                      ),
+                                    ],
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {},
+                                    child: Container(
+                                        margin: EdgeInsets.only(right: 15),
+                                        child: Icon(
+                                          Icons.favorite_border_outlined,
+                                          color: Colors.white70,
+                                          size: 28,
+                                        )),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Flexible(
+                                        child: Container(
+                                          margin: EdgeInsets.only(
+                                              bottom: 15,
+                                              left: 10,
+                                              right: 10,
+                                              top: 5),
+                                          child: Text(
+                                              filteredCommunities[index].getDescription(),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 2,
+                                              textAlign: TextAlign.left,
+                                              style: TextStyle(
+                                                  fontFamily: 'Gotham',
+                                                  fontSize: 12,
+                                                  color: Color.fromARGB(
+                                                      177, 255, 255, 255))),
+                                        )),
+                                    Container(
+                                        width: 1,
+                                        height: 1,
+                                        color: Colors.transparent)
+                                  ]),
+                              Container(height: 1, color: Colors.black54)
+                            ],
+                          ))));
+            }),
+      ),
+        backgroundColor: Color(0xFF1F2833),
+    );
   }
 }
